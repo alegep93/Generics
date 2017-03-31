46fd9e22120bd232e1b03b0507f3a814ad27f96d
Alessandro <geppi.alessandro@gmail.com>
2017-03-31 15:03:49 +0200

    585

--- src/com/prova/generics/Main.java
+++ src/com/prova/generics/Main.java
@@ -12,12 +12,12 @@ public class Main {
 		Scanner scan = new Scanner(System.in);
 		String input = "";	
 		int counter = 0;
-        Persona[] pList = new Persona[2];
-        MyArray<Persona> myA = new MyArray<>(pList);
-        
-        printMsg();
-                        
-        while(!("q".equals(input = scan.nextLine()))){
+		Persona[] pList = new Persona[2];
+		MyArray<Persona> myA = new MyArray<>(pList);
+
+		printMsg();
+
+		while(!("q".equals(input = scan.nextLine()))){
 			switch(input){
 				case "n":
 					if (counter<pList.length)
@@ -26,50 +26,63 @@ public class Main {
 						System.out.println("Lista già piena");
 					printMsg();
 					counter++;
-				break;
+					break;
 				case "r":
 					myA.pop();
 					printMsg();
 					counter--;
-				break;
+					break;
+				case "v":
+					if(counter != 0){
+						for(int i=0; i<counter; i++){
+							pList[i].printPersona();
+							System.out.format("%-16s %s \r\n", "Posizione: ", i);
+						}
+					}else{
+						System.out.println("------------------------------------------------");
+						System.out.println("  Non sono ancora state inserite delle persone");
+					}
+					printMsg();
+					break;
 				default:
 					System.err.println("Comando inserito non valido, scegli un comando tra quelli disponibili!");
 					printMsg();
-				break;
+					break;
 			}
 		}
 		System.out.println("Esecuzione Terminata!");
 		scan.close();
 	}	
-	
+
 	public static void printMsg(){		
 		System.out.println("------------------------------------------------");
 		System.out.println("Scegli un'azione tra le seguenti: ");
 		System.out.println("  n => Inserisci una nuova persona");
 		System.out.println("  r => Rimuovi una persona");
+		System.out.println("  v => Visualizza la lista delle persone inserite");
 		System.out.println("  q => Termina il programma");
 	}
-	
+
 	public static void InserisciPersona(Persona[] pList, MyArray<Persona> myA, Scanner scan){
 		String nome = "", cognome = "", cf = "";
-		
+
 		System.out.println("Inserisci il nome della persona da inserire:");
-        nome = scan.nextLine();
-        
-        System.out.println("Inserisci il cognome della persona da inserire:");
-        cognome = scan.nextLine();
-        
-        System.out.println("Inserisci il Codice Fiscale della persona da inserire:");
-        cf = scan.nextLine();
-        
-        while(!controllaCF(cf)){
-        	System.out.println("Codice Fiscale non corretto, ritenta:");
-            cf = scan.nextLine();
-        }
-        
-        myA.push(new Persona(nome, cognome, cf));        
+		nome = scan.nextLine();
+
+		System.out.println("Inserisci il cognome della persona da inserire:");
+		cognome = scan.nextLine();
+
+		System.out.println("Inserisci il Codice Fiscale della persona da inserire:");
+		cf = scan.nextLine();
+
+		while(!controllaCF(cf)){
+			System.out.println("Codice Fiscale non corretto, ritenta:");
+			cf = scan.nextLine();
+		}
+
+		myA.push(new Persona(nome, cognome, cf));        
 	}
-	
+
 	public static boolean controllaCF(String cf){
 		Pattern cfRegExp = Pattern.compile("[A-Za-z]{6}[0-9]{2}[A-Za-z]{1}[0-9]{2}[A-Za-z0-9]{4}[A-Za-z]{1}");
 		Matcher match = cfRegExp.matcher(cf);
